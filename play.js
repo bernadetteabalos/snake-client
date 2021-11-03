4b9651cdc30981ba270aa48bfba48fc548f8338c
Bernadette Abalos <bern.roseabalos@gmail.com>
2021-11-03 19:48:36 +0000

    Created setupinput and handleuserinput functions

--- play.js
+++ play.js
@@ -5,3 +5,23 @@ const connect = require('./client');
 
 console.log("Connecting ...");
 connect();
+
+// setup interface to handle user input from stdin
+
+const stdin = process.stdin;
+const setupInput = function () {
+  stdin.setRawMode(true);
+  stdin.setEncoding("utf8");
+  stdin.resume();
+  stdin.on("data", handleUserInput);
+  return stdin;
+};
+
+const handleUserInput = function () {
+  stdin.on("data", (key) => {
+    if (key === '\u0003') {
+      process.exit();
+    }
+})};
+
+setupInput();
